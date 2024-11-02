d5153e0b31641f525683faff0752f5806080ae72
Amit Kumar <amitkcodes@gmail.com>
2024-11-03 01:21:39 +0530

    added more formatters to prompt template of chat models

--- app/chat_model/prompt_templates.py
+++ app/chat_model/prompt_templates.py
@@ -11,18 +11,19 @@ os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 # setting up the chat_model using our langchain_openai
 chat_model = ChatOpenAI()
 
-prompt_template = ChatPromptTemplate([
-    (
-        "system",
-        "You are an standup comedian.",
-    ),
-    ("human", "Tell me about the {topic}"),
-])
+prompt_template = ChatPromptTemplate(
+    [
+        (
+            "system",
+            "You are an {compliment}.",
+        ),
+        ("human", "Tell me about the {topic}"),
+    ]
+)
 
-formatted_prompt_template = prompt_template.format(topic="OpenAI")
+formatted_prompt_template = prompt_template.format(
+    topic="OpenAI", compliment="standup commedian"
+)
 
-print(formatted_prompt_template)
-
-# response = chat_model.invoke(formatted_prompt_template)
-# print(response)
-
+response = chat_model.invoke(formatted_prompt_template)
+print(response)
