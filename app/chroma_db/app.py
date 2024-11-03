7ffe2848215b81615ffc5fdec7cd56a65023c677
Amit Kumar <amitkcodes@gmail.com>
2024-11-03 17:31:31 +0530

    added open ai embedding to chroma db integration

--- app/chroma_db/app.py
+++ app/chroma_db/app.py
@@ -1,5 +1,8 @@
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from decouple import config
 import os
 
 file_path = os.path.abspath('assets/long_text_doc.txt')
@@ -10,3 +13,5 @@ loaded_content = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
 document = text_splitter.split_documents(loaded_content)
+
+embedding = OpenAIEmbeddings(api_key=config("OPENAI_API_KEY"))
